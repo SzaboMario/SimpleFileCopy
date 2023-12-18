b40e6b189bb0e82a56ebfd2201f88234eee12533
SzaboMario <szabo.mario.93@gmail.com>
2023-12-18 20:05:29 +0100

    SimpleFileCopy beta init

---
+++ SimpleFileCopy/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace SimpleFileCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sourceFolderText = new System.Windows.Forms.TextBox();
+            this.targetFolderText = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.timeIntervalText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sourceOpenBtn = new System.Windows.Forms.Button();
+            this.targetOpenBtn = new System.Windows.Forms.Button();
+            this.runIndicatorPic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.runIndicatorPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source folder:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Target folder:";
+            // 
+            // sourceFolderText
+            // 
+            this.sourceFolderText.Location = new System.Drawing.Point(91, 6);
+            this.sourceFolderText.Name = "sourceFolderText";
+            this.sourceFolderText.Size = new System.Drawing.Size(278, 20);
+            this.sourceFolderText.TabIndex = 2;
+            this.sourceFolderText.TextChanged += new System.EventHandler(this.sourceFolderText_TextChanged);
+            // 
+            // targetFolderText
+            // 
+            this.targetFolderText.Location = new System.Drawing.Point(91, 36);
+            this.targetFolderText.Name = "targetFolderText";
+            this.targetFolderText.Size = new System.Drawing.Size(278, 20);
+            this.targetFolderText.TabIndex = 3;
+            this.targetFolderText.TextChanged += new System.EventHandler(this.targetFolderText_TextChanged);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(375, 68);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 34);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // timeIntervalText
+            // 
+            this.timeIntervalText.Location = new System.Drawing.Point(133, 75);
+            this.timeIntervalText.Name = "timeIntervalText";
+            this.timeIntervalText.Size = new System.Drawing.Size(95, 20);
+            this.timeIntervalText.TabIndex = 6;
+            this.timeIntervalText.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Copy time interval (ms):";
+            // 
+            // sourceOpenBtn
+            // 
+            this.sourceOpenBtn.Location = new System.Drawing.Point(375, 6);
+            this.sourceOpenBtn.Name = "sourceOpenBtn";
+            this.sourceOpenBtn.Size = new System.Drawing.Size(128, 20);
+            this.sourceOpenBtn.TabIndex = 7;
+            this.sourceOpenBtn.Text = "Select source folder";
+            this.sourceOpenBtn.UseVisualStyleBackColor = true;
+            this.sourceOpenBtn.Click += new System.EventHandler(this.sourceOpenBtn_Click);
+            // 
+            // targetOpenBtn
+            // 
+            this.targetOpenBtn.Location = new System.Drawing.Point(375, 36);
+            this.targetOpenBtn.Name = "targetOpenBtn";
+            this.targetOpenBtn.Size = new System.Drawing.Size(128, 20);
+            this.targetOpenBtn.TabIndex = 8;
+            this.targetOpenBtn.Text = "Select target folder";
+            this.targetOpenBtn.UseVisualStyleBackColor = true;
+            this.targetOpenBtn.Click += new System.EventHandler(this.targetOpenBtn_Click);
+            // 
+            // runIndicatorPic
+            // 
+            this.runIndicatorPic.BackColor = System.Drawing.Color.Tomato;
+            this.runIndicatorPic.Location = new System.Drawing.Point(470, 77);
+            this.runIndicatorPic.Name = "runIndicatorPic";
+            this.runIndicatorPic.Size = new System.Drawing.Size(19, 18);
+            this.runIndicatorPic.TabIndex = 9;
+            this.runIndicatorPic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 112);
+            this.Controls.Add(this.runIndicatorPic);
+            this.Controls.Add(this.targetOpenBtn);
+            this.Controls.Add(this.sourceOpenBtn);
+            this.Controls.Add(this.timeIntervalText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.targetFolderText);
+            this.Controls.Add(this.sourceFolderText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple File Copy";
+            ((System.ComponentModel.ISupportInitialize)(this.runIndicatorPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sourceFolderText;
+        private System.Windows.Forms.TextBox targetFolderText;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.TextBox timeIntervalText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button sourceOpenBtn;
+        private System.Windows.Forms.Button targetOpenBtn;
+        private System.Windows.Forms.PictureBox runIndicatorPic;
+    }
+}
+
---
+++ SimpleFileCopy/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFileCopy
+{
+    public partial class Form1 : Form
+    {
+        string sourceFolder, targetFolder;
+        static int timeInterval = 5;
+        bool start = false;
+        static StreamWriter writer;
+        string reportName = "";
+        static int fileCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timeIntervalText.Text = timeInterval.ToString();           
+        }
+        
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(timeIntervalText.Text)==0)
+                {
+                    timeInterval = 5;
+                    timeIntervalText.Text = timeInterval.ToString();
+                }
+                if (System.Text.RegularExpressions.Regex.IsMatch(timeIntervalText.Text, "[^0-9]"))
+                {
+                    MessageBox.Show("Please enter only numbers.");
+                    timeIntervalText.Text = timeIntervalText.Text.Remove(timeIntervalText.Text.Length - 1);
+                }
+                else if (timeIntervalText.Text.Length>0)
+                {
+                    timeInterval = Convert.ToInt32(timeIntervalText.Text);
+                }
+                else
+                {
+                    timeInterval = 5;
+                    timeIntervalText.Text = timeInterval.ToString();
+                }
+            }
+            catch (Exception)
+            {
+                    MessageBox.Show("Not correct format!");
+                    timeInterval = 5;
+                    timeIntervalText.Text = timeInterval.ToString();                          
+            }                  
+        }
+
+        private void targetOpenBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                targetFolder = dlg.SelectedPath;
+                targetFolderText.Text = targetFolder;
+            }
+        }
+
+        
+
+        private void sourceOpenBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                sourceFolder = dlg.SelectedPath;
+                sourceFolderText.Text = sourceFolder;
+            }
+        }
+
+
+
+        private void Stop()
+        {         
+            start = false;
+            if (startBtn.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    startBtn.Text = "Start";
+                    startBtn.ForeColor = Color.Green;
+                    runIndicatorPic.BackColor = Color.Tomato;
+                    timeIntervalText.Enabled = true;
+                    sourceFolderText.Enabled = true;
+                    targetFolderText.Enabled = true;
+                });
+            }
+            if (fileCounter>0)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+                result = MessageBox.Show("Finished!\nReport completed during the program!\nDo you want to open it?", "Result", buttons);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    var p = new Process();
+                    p.StartInfo.FileName = reportName;  // just for example, you can use yours.
+                    p.Start();
+                }
+            }
+           
+        }
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (sourceFolderText.Text.Length>0&&targetFolderText.Text.Length>0)
+            {
+                if (start == false)
+                {
+                    reportName = $"report_{DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")}.txt";
+                    writer = new StreamWriter(reportName);
+                    start = true;
+                    Thread t1 = new Thread(new ThreadStart(th_1));
+                    t1.IsBackground = true;
+                    t1.Start();
+                    startBtn.Text = "Stop";
+                    startBtn.ForeColor = Color.Red;
+                    runIndicatorPic.BackColor = Color.Green;
+                    timeIntervalText.Enabled = false;
+                    sourceFolderText.Enabled = false;
+                    targetFolderText.Enabled = false;
+                }
+                else
+                {
+                    timeIntervalText.Enabled = true;
+                    sourceFolderText.Enabled = true;
+                    targetFolderText.Enabled = true;
+                    string message = "Do you want to stop the process?";
+                    string caption = "stop";
+                    MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                    DialogResult result;
+                    result = MessageBox.Show(message, caption, buttons);
+                    if (result == System.Windows.Forms.DialogResult.Yes)
+                    {
+                        start = false;
+                        startBtn.Text = "Start";
+                        startBtn.ForeColor = Color.Green;
+                        runIndicatorPic.BackColor = Color.Tomato;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Empty path! Please fill it.");
+            }                    
+        }
+
+        private void th_1()
+        {
+            while (start)
+            {
+                try
+                {
+                    string[] files = Directory.GetFiles(sourceFolder);
+                    DirectoryCopy(sourceFolder, targetFolder, true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }                           
+                Stop();              
+            }
+            writer.Flush();
+            writer.Close();
+        }
+
+        private void sourceFolderText_TextChanged(object sender, EventArgs e)
+        {
+             sourceFolder= sourceFolderText.Text;
+        }
+
+        private void targetFolderText_TextChanged(object sender, EventArgs e)
+        {
+            targetFolder = targetFolderText.Text;
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName,
+                                      bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {            
+                try
+                {
+                    string temppath = Path.Combine(destDirName, file.Name);
+                    file.CopyTo(temppath, false);
+                    fileCounter++;
+                    writer.WriteLine(fileCounter+$". Time: {DateTime.Now.ToString()} Copy OK: "+ temppath);
+                }
+                catch (Exception ex)
+                {
+                   // MessageBox.Show(ex.Message);
+                }               
+                Thread.Sleep(timeInterval);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    //string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, destDirName, copySubDirs);                  
+                }
+            }
+        }
+    }
+}
